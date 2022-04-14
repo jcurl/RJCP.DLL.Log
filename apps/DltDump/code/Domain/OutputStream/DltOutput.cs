658c9182fbf7974c71b4968d70104ec6781c0346
Jason Curl <jcurl@arcor.de>
2022-04-14 17:50:03 +0200

    DltDump: Implement DltOutput module
    The DltOutput is the module that will be responsible for writing binary DLT data, as it's decoded inside the decoder.
    
    Issue: DOTNET-610

---
+++ apps/DltDump/code/Domain/OutputStream/DltOutput.cs
@@ -0,0 +1,102 @@
+﻿namespace RJCP.App.DltDump.Domain.OutputStream
+{
+    using System;
+    using Infrastructure.Dlt;
+    using RJCP.Diagnostics.Log.Dlt;
+
+    /// <summary>
+    /// An output module that can write binary data as it is received.
+    /// </summary>
+    public sealed class DltOutput : OutputBase, IOutputStream
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DltOutput"/> class.
+        /// </summary>
+        /// <param name="fileName">Name of the file.</param>
+        /// <exception cref="ArgumentNullException"><paramref name="fileName"/> is <see langword="null"/>.</exception>
+        /// <exception cref="ArgumentException"><paramref name="fileName"/> is empty.</exception>
+        public DltOutput(string fileName) : base(fileName) { }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DltOutput"/> class.
+        /// </summary>
+        /// <param name="fileName">Name of the file.</param>
+        /// <param name="force">Force overwrite the file if <see langword="true"/>.</param>
+        /// <exception cref="ArgumentNullException"><paramref name="fileName"/> is <see langword="null"/>.</exception>
+        /// <exception cref="ArgumentException"><paramref name="fileName"/> is empty.</exception>
+        public DltOutput(string fileName, bool force) : base(fileName, 0, force) { }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DltOutput"/> class.
+        /// </summary>
+        /// <param name="fileName">Name of the file.</param>
+        /// <param name="split">The number of bytes to write before splitting.</param>
+        /// <param name="force">Force overwrite the file if <see langword="true"/>.</param>
+        /// <exception cref="ArgumentNullException"><paramref name="fileName"/> is <see langword="null"/>.</exception>
+        /// <exception cref="ArgumentException"><paramref name="fileName"/> is empty.</exception>
+        public DltOutput(string fileName, long split, bool force) : base(fileName, split, force) { }
+
+        /// <summary>
+        /// Indicates if this output stream can write binary data.
+        /// </summary>
+        /// <value>
+        /// Is <see langword="true"/> if this object can write binary data; otherwise, <see langword="false"/>.
+        /// </value>
+        public bool SupportsBinary { get { return true; } }
+
+        /// <summary>
+        /// Defines the input file name and the format of the input file.
+        /// </summary>
+        /// <param name="fileName">Name of the input file.</param>
+        /// <param name="inputFormat">The input format.</param>
+        /// <exception cref="ArgumentNullException">fileName</exception>
+        /// <exception cref="ArgumentOutOfRangeException">inputFormat</exception>
+        /// <remarks>
+        /// Setting the input file name and the format can assist with knowing how to write binary data, and optionally
+        /// set the name of the file that should be written.
+        /// </remarks>
+        public void SetInput(string fileName, InputFormat inputFormat)
+        {
+            if (fileName == null) throw new ArgumentNullException(nameof(fileName));
+            if (!Enum.IsDefined(typeof(InputFormat), inputFormat))
+                throw new ArgumentOutOfRangeException(nameof(inputFormat));
+            if (inputFormat == InputFormat.Automatic)
+                throw new ArgumentOutOfRangeException(nameof(inputFormat));
+
+            if (inputFormat != InputFormat.File)
+                throw new NotImplementedException();
+
+            SetInput(fileName);
+        }
+
+        /// <summary>
+        /// Writes the specified line to the output.
+        /// </summary>
+        /// <param name="line">The line to write.</param>
+        /// <returns>
+        /// Returns <see langword="true"/> if after writing this line was written. A pure writer would not filter any
+        /// lines. Is <see langword="false"/> if the line was filtered out.
+        /// </returns>
+        public bool Write(DltTraceLineBase line)
+        {
+            /* Not yet implemented */
+            return false;
+        }
+
+        /// <summary>
+        /// Writes the specified line to the output.
+        /// </summary>
+        /// <param name="line">The line to write.</param>
+        /// <param name="packet">The original packet data that generated the line to help write the output.</param>
+        /// <returns>
+        /// Returns <see langword="true"/> if after writing this line was written. A pure writer would not filter any
+        /// lines. Is <see langword="false"/> if the line was filtered out.
+        /// </returns>
+        /// <remarks>The output knows of the input format through the method <see cref="SetInput"/>.</remarks>
+        public bool Write(DltTraceLineBase line, ReadOnlySpan<byte> packet)
+        {
+            Write(line.TimeStamp, packet);
+            return true;
+        }
+    }
+}
---
+++ apps/DltDump/test/Domain/OutputStream/DltOutputTest.cs
@@ -0,0 +1,146 @@
+﻿namespace RJCP.App.DltDump.Domain.OutputStream
+{
+    using System;
+    using System.IO;
+    using Infrastructure.Dlt;
+    using NUnit.Framework;
+    using RJCP.CodeQuality.NUnitExtensions;
+    using TestResources;
+
+    [TestFixture]
+    public class DltOutputTest
+    {
+        [Test]
+        public void NullFileName()
+        {
+            Assert.That(() => {
+                _ = new DltOutput(null);
+            }, Throws.TypeOf<ArgumentNullException>());
+        }
+
+        [Test]
+        public void EmptyFileName()
+        {
+            Assert.That(() => {
+                _ = new DltOutput(string.Empty);
+            }, Throws.TypeOf<ArgumentException>());
+        }
+
+        [Test]
+        public void DefaultProperties()
+        {
+            using (ScratchPad pad = Deploy.ScratchPad())
+            using (DltOutput output = new DltOutput("File.dlt")) {
+                Assert.That(File.Exists("File.dlt"), Is.False);
+                Assert.That(output.SupportsBinary, Is.True);
+                Assert.That(output.Force, Is.False);
+            }
+        }
+
+        [Test]
+        public void DefaultPropertiesWithForce()
+        {
+            using (ScratchPad pad = Deploy.ScratchPad())
+            using (DltOutput output = new DltOutput("File.dlt", true)) {
+                Assert.That(File.Exists("File.dlt"), Is.False);
+                Assert.That(output.SupportsBinary, Is.True);
+                Assert.That(output.Force, Is.True);
+            }
+        }
+
+        [Test]
+        public void WriteLineAsPacket()
+        {
+            byte[] data = new byte[] { 0x35 };
+
+            using (ScratchPad pad = Deploy.ScratchPad())
+            using (DltOutput output = new DltOutput("File.dlt")) {
+                Assert.That(File.Exists("File.dlt"), Is.False);
+
+                // If this writes binary data, it would just write 1 byte, not the length.
+                output.Write(TestLines.Verbose, data.AsSpan());
+                output.Flush();
+
+                Assert.That(File.Exists("File.dlt"), Is.True);
+
+                // The data is written exactly as the packet says. There is no interpretation of the data.
+                FileInfo fileInfo = new FileInfo("File.dlt");
+                Assert.That(fileInfo.Length, Is.EqualTo(1));
+            }
+        }
+
+        [Test]
+        public void WriteLinesAsPacket()
+        {
+            byte[] data = new byte[] { 0x35, 0x00 };
+
+            using (ScratchPad pad = Deploy.ScratchPad())
+            using (DltOutput output = new DltOutput("File.dlt")) {
+                Assert.That(File.Exists("File.dlt"), Is.False);
+
+                // If this writes binary data, it would just write 1 byte, not the length.
+                output.Write(TestLines.Verbose, data.AsSpan());
+                output.Write(TestLines.Verbose2, data.AsSpan());
+                output.Flush();
+
+                Assert.That(File.Exists("File.dlt"), Is.True);
+
+                // The data is written exactly as the packet says. There is no interpretation of the data.
+                FileInfo fileInfo = new FileInfo("File.dlt");
+                Assert.That(fileInfo.Length, Is.EqualTo(4));
+            }
+        }
+
+        [Test]
+        public void SetInputNullString()
+        {
+            using (ScratchPad pad = Deploy.ScratchPad())
+            using (DltOutput output = new DltOutput("File.dlt")) {
+                Assert.That(File.Exists("File.dlt"), Is.False);
+
+                Assert.That(() => {
+                    output.SetInput(null, InputFormat.File);
+                }, Throws.TypeOf<ArgumentNullException>());
+            }
+        }
+
+        [Test]
+        public void SetInputUnknownFormat()
+        {
+            using (ScratchPad pad = Deploy.ScratchPad())
+            using (DltOutput output = new DltOutput("File.dlt")) {
+                Assert.That(File.Exists("File.dlt"), Is.False);
+
+                Assert.That(() => {
+                    output.SetInput("input.dlt", (InputFormat)255);
+                }, Throws.TypeOf<ArgumentOutOfRangeException>());
+            }
+        }
+
+        [Test]
+        public void SetInputUnsupportedFormatAutomatic()
+        {
+            using (ScratchPad pad = Deploy.ScratchPad())
+            using (DltOutput output = new DltOutput("File.dlt")) {
+                Assert.That(File.Exists("File.dlt"), Is.False);
+
+                Assert.That(() => {
+                    output.SetInput("input.dlt", InputFormat.Automatic);
+                }, Throws.TypeOf<ArgumentOutOfRangeException>());
+            }
+        }
+
+        [Test]
+        public void SetInputValid()
+        {
+            using (ScratchPad pad = Deploy.ScratchPad())
+            using (DltOutput output = new DltOutput("File.dlt")) {
+                Assert.That(File.Exists("File.dlt"), Is.False);
+                output.SetInput("input.dlt", InputFormat.File);
+
+                // The file is still only created when written.
+                Assert.That(File.Exists("File.dlt"), Is.False);
+            }
+        }
+    }
+}
