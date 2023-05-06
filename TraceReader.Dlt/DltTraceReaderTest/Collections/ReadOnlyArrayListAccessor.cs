14fed997a8f619147eb97bfbedf4ab4a0f282d26
Jason Curl <jcurl@arcor.de>
2023-05-07 01:30:21 +0200

    DLT: Test the `ReadOnlyArrayList` internal object
    Issue: DOTNET-754

---
+++ TraceReader.Dlt/DltTraceReaderTest/Collections/ReadOnlyArrayListAccessor.cs
@@ -0,0 +1,78 @@
+﻿namespace RJCP.Diagnostics.Log.Collections
+{
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+    using RJCP.CodeQuality;
+
+    public class ReadOnlyArrayListAccessor<T> : AccessorBase, IList<T>
+    {
+        private const string AssemblyName = "RJCP.Diagnostics.Log.Dlt";
+        private const string TypeName = "RJCP.Diagnostics.Log.Collections.ReadOnlyArrayList`1";
+        private static readonly PrivateType AccType =
+            new PrivateType(AssemblyName, TypeName, new Type[] { typeof(T) });
+
+        public ReadOnlyArrayListAccessor(T[] array)
+            : base(AccType, new Type[] { typeof(T[]) }, new object[] { array }) { }
+
+        public T this[int index]
+        {
+            get { return GetProperty<T>("Item", index); }
+            set { SetProperty("Item", value, index); }
+        }
+
+        public int Count { get { return GetProperty<int>(nameof(Count)); } }
+
+        public bool IsReadOnly { get { return GetProperty<bool>(nameof(IsReadOnly)); } }
+
+        public void Add(T item)
+        {
+            Invoke(nameof(Add), new Type[] { typeof(T) }, new object[] { item });
+        }
+
+        public void Clear()
+        {
+            Invoke(nameof(Clear));
+        }
+
+        public bool Contains(T item)
+        {
+            return (bool)Invoke(nameof(Contains), new Type[] { typeof(T) }, new object[] { item });
+        }
+
+        public void CopyTo(T[] array, int arrayIndex)
+        {
+            Invoke(nameof(CopyTo), new Type[] { typeof(T[]), typeof(int) }, new object[] { array, arrayIndex });
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return (IEnumerator<T>)Invoke(nameof(GetEnumerator));
+        }
+
+        public int IndexOf(T item)
+        {
+            return (int)Invoke(nameof(IndexOf), new Type[] { typeof(T) }, new object[] { item });
+        }
+
+        public void Insert(int index, T item)
+        {
+            Invoke(nameof(Insert), new Type[] { typeof(int), typeof(T) }, new object[] { index, item });
+        }
+
+        public bool Remove(T item)
+        {
+            return (bool)Invoke(nameof(Remove), new Type[] { typeof(T) }, new object[] { item });
+        }
+
+        public void RemoveAt(int index)
+        {
+            Invoke(nameof(RemoveAt), index);
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return (IEnumerator)Invoke(nameof(GetEnumerator));
+        }
+    }
+}
---
+++ TraceReader.Dlt/DltTraceReaderTest/Collections/ReadOnlyArrayListTest.cs
@@ -0,0 +1,149 @@
+﻿namespace RJCP.Diagnostics.Log.Collections
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class ReadOnlyArrayListTest
+    {
+        [Test]
+        public void NullArray()
+        {
+            Assert.That(() => {
+                _ = new ReadOnlyArrayListAccessor<int>(null);
+            }, Throws.TypeOf<ArgumentNullException>());
+        }
+
+        [Test]
+        public void Length()
+        {
+            int[] array = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            ReadOnlyArrayListAccessor<int> list = new ReadOnlyArrayListAccessor<int>(array);
+
+            Assert.That(list.Count, Is.EqualTo(array.Length));
+            Assert.That(list.IsReadOnly, Is.True);
+        }
+
+        [Test]
+        public void GetItems()
+        {
+            int[] array = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            ReadOnlyArrayListAccessor<int> list = new ReadOnlyArrayListAccessor<int>(array);
+
+            Assert.That(list[0], Is.EqualTo(1));
+            Assert.That(list[8], Is.EqualTo(9));
+        }
+
+        [Test]
+        public void SetItems()
+        {
+            int[] array = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            ReadOnlyArrayListAccessor<int> list = new ReadOnlyArrayListAccessor<int>(array);
+
+            Assert.That(() => {
+                list[0] = 0;
+            }, Throws.TypeOf<NotSupportedException>());
+        }
+
+        [Test]
+        public void Add()
+        {
+            int[] array = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            ReadOnlyArrayListAccessor<int> list = new ReadOnlyArrayListAccessor<int>(array);
+
+            Assert.That(() => {
+                list.Add(42);
+            }, Throws.TypeOf<NotSupportedException>());
+        }
+
+        [Test]
+        public void Clear()
+        {
+            int[] array = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            ReadOnlyArrayListAccessor<int> list = new ReadOnlyArrayListAccessor<int>(array);
+
+            Assert.That(() => {
+                list.Clear();
+            }, Throws.TypeOf<NotSupportedException>());
+        }
+
+        [Test]
+        public void Contains()
+        {
+            int[] array = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            ReadOnlyArrayListAccessor<int> list = new ReadOnlyArrayListAccessor<int>(array);
+
+            Assert.That(list.Contains(1), Is.True);
+            Assert.That(list.Contains(42), Is.False);
+            Assert.That(list.Contains(0), Is.False);
+        }
+
+        [Test]
+        public void IndexOf()
+        {
+            int[] array = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            ReadOnlyArrayListAccessor<int> list = new ReadOnlyArrayListAccessor<int>(array);
+
+            Assert.That(list.IndexOf(1), Is.EqualTo(0));
+            Assert.That(list.IndexOf(9), Is.EqualTo(8));
+            Assert.That(list.IndexOf(42), Is.EqualTo(-1));
+            Assert.That(list.IndexOf(0), Is.EqualTo(-1));
+        }
+
+        [Test]
+        public void Insert()
+        {
+            int[] array = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            ReadOnlyArrayListAccessor<int> list = new ReadOnlyArrayListAccessor<int>(array);
+
+            Assert.That(() => {
+                list.Insert(0, 42);
+            }, Throws.TypeOf<NotSupportedException>());
+        }
+
+        [Test]
+        public void Remove()
+        {
+            int[] array = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            ReadOnlyArrayListAccessor<int> list = new ReadOnlyArrayListAccessor<int>(array);
+
+            Assert.That(() => {
+                list.Remove(5);
+            }, Throws.TypeOf<NotSupportedException>());
+        }
+
+        [Test]
+        public void RemoveAt()
+        {
+            int[] array = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            ReadOnlyArrayListAccessor<int> list = new ReadOnlyArrayListAccessor<int>(array);
+
+            Assert.That(() => {
+                list.RemoveAt(5);
+            }, Throws.TypeOf<NotSupportedException>());
+        }
+
+        [Test]
+        public void CopyTo()
+        {
+            int[] array = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            ReadOnlyArrayListAccessor<int> list = new ReadOnlyArrayListAccessor<int>(array);
+
+            int[] cpArray = new int[array.Length];
+            list.CopyTo(cpArray, 0);
+
+            Assert.That(cpArray, Is.EqualTo(array));
+        }
+
+        [Test]
+        public void Enumerator()
+        {
+            int[] array = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            ReadOnlyArrayListAccessor<int> list = new ReadOnlyArrayListAccessor<int>(array);
+
+            Assert.That(list, Is.EqualTo(array));
+        }
+    }
+}
