5c7e7571f099181af068c4d229b3e3f970c20374
Jason Curl <jcurl@arcor.de>
2023-11-19 22:04:31 +0100

    DltDump: Fix test case to support .NET 8.0 and later
    With the upgrade of .NET 8.0, if a project has a version, but no informational version, then .NET SDK 8.0 will add the string "+githash" to the end.
    
    Issue: DOTNET-879

--- apps/DltDump/test/Infrastructure/VersionTest.cs
+++ apps/DltDump/test/Infrastructure/VersionTest.cs
@@ -8,16 +8,22 @@
         [Test]
         public void GetAssemblyVersion()
         {
+            // Note, we're testing `Version.GetAssemblyVersion`, not the actual DltDump program version.
             string version = Version.GetAssemblyVersion(typeof(VersionTest));
 
             // This is the version in the .csproj file. There's no easy way to test the two use cases between
             // information version, and the assembly version.
-            Assert.That(version, Is.EqualTo("1.0.0"));
+
+            // When building, the version depends on the SDK that is being used (not the runtime):
+            // * On .NET Core 7.x and earlier, the Informational version is "1.0.0".
+            // * On .NET Core 8.0 and later, the Informational version is "1.0.0+githash".
+            Assert.That(version, Is.EqualTo("1.0.0").Or.StartsWith("1.0.0+"));
         }
 
         [Test]
         public void GetAssemblyCopyright()
         {
+            // Note, we're testing `Version.GetAssemblyVersion`, not the actual DltDump program version.
             string copyright = Version.GetAssemblyCopyright(typeof(VersionTest));
 
             Assert.That(copyright, Is.EqualTo("(C) 2022-2023, Jason Curl"));
